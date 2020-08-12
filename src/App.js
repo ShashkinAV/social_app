695287ba35271f2e79fa3c25e846bdade5f8c67e
Shashkin Anton <aveksis@gmail.com>
2020-08-12 17:17:05 +0300

    header component
    add header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
 
 function App(props) {
   return (
     <div className="app-wrapper">
       <Header />
-      <Sidebar />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import Avatar from "../../img/profile.jpg";
 
 const Header = (props) => {
   return (
-    <header className="header fixed bg-white d-lg-block d-none">
+    <header className="header fixed d-lg-block d-none">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-md-5">
+          <div className="col-md-5 d-flex">
             <nav className="menu">
               <ul className="menu__list">
                 <li className="menu__item">
@@ -28,16 +28,19 @@ const Header = (props) => {
               </ul>
             </nav>
           </div>
-          <div className="col-md-2">
+          <div className="col-md-2 d-flex justify-content-center">
             <div className="brand">
               <a href="/home" className="brand__link">
                 <img className="brand__img" src={Logo} alt="Logo" />
               </a>
             </div>
           </div>
-          <div className="col-md-5">
-              <form action="" className="search">
-                  <input type="search" className="search__input"/>
+          <div className="col-md-5 d-flex justify-content-end align-items-center">
+              <form className="search">
+                  <input type="search" className="search__input" placeholder="Search"/>
+				  <button className="search__btn">
+					  <i className="icon-search"></i>
+				  </button>
               </form>
               <div className="login">
                   <img src={Avatar} alt="Avatar" className="login__avatar"/>
