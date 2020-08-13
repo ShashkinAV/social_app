b7b4bd6f083d3eb37ef9a10717b6c9328cd672ac
Shashkin Anton <aveksis@gmail.com>
2020-08-13 19:42:22 +0300

    add post component
    верстка поста, компонента добавление поста

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ProfileImg from "../../img/profile-midle-1.jpg";
+import PostProfileImg from "../../img/profile.jpg";
+import PostGallery from "../../img/post-1.jpg";
 
 const Post = (props) => {
   return (
@@ -17,7 +19,33 @@ const Post = (props) => {
             </form>
         </div>
     </div>
-      <div className="post">Posts</div>
+      <div className="post">
+		  	<div className="post__title d-flex align-items-center">
+				<div className="post__thumb">
+					<a href="/profile">
+						<figure className="post__img">
+							<img src={PostProfileImg} alt="Profile"/>
+						</figure>
+					</a>
+				</div>
+				<div className="post__author">
+					<h2  className="author">John Watson</h2>
+					<span className="post__time">20 min ago</span>
+				</div>
+				<div className="post__edit"></div>
+			</div>
+			<div className="post__content">
+				<p className="post__desc">
+				Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+				</p>
+				<div className="post__gallary">
+					<img src={PostGallery} alt="gallery"/>
+				</div>
+				<div className="post__meta">
+					<button className="post__like">Like this post</button>
+				</div>
+			</div>
+	  </div>
     </div>
   );
 };
